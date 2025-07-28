11d23177a20efe236631c47bc9d5da4d5772a834
rajswi19 <rajswi19@gmail.com>
2025-07-29 03:03:25 +0530

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -19,7 +19,7 @@ export const Navbar =()=> {
         <header className='fixed z-50 w-full border-b '>
             <nav
                 data-state={menuState && 'active'}
-                className="bg-background/50 fixed z-20 w-full border-b backdrop-blur-sm">
+                className="bg-background fixed z-20 w-full border-b backdrop-blur-sm">
                 <div className="mx-auto max-w-6xl px-6 transition-all duration-300">
                     <div className="relative flex flex-wrap items-center justify-between gap-6 py-3 lg:gap-0 lg:py-4">
                         <div className="flex w-full items-center justify-between gap-12 lg:w-auto">
